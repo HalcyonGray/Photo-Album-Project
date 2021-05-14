530014679017afbf52a08cd489ca1bd21bf97649
Grayson Smith <gsfireguy41@gmail.com>
2021-05-14 12:05:28 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -10,3 +10,6 @@ reqs = subprocess.check_output([sys.executable, '-m', 'pip',
 'freeze'])
 installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
 print(installed_packages)
+print()
+print('Finished Installing packages, Close and Run Start')
+
