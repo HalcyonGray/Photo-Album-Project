62512a6f0bd23a651e235d0e180762f4ca344057
Grayson Smith <gsfireguy41@gmail.com>
2021-05-14 12:44:07 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -6,10 +6,9 @@ subprocess.run('pip install image-quality')
 subprocess.run('pip install pysqlite3')
 
 #Lists all installed Python Packages
-reqs = subprocess.check_output([sys.executable, '-m', 'pip',
-'freeze'])
-installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
-print(installed_packages)
+#reqs = subprocess.check_output([sys.executable, '-m', 'pip','freeze'])
+#installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+#print(installed_packages)
 print()
 print('Finished Installing packages, Close and Run Start')
 
